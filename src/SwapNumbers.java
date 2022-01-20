7d8eb99f42f0b88d5542320267ccfe922a0e736b
Rabin Awal <robinleathal@gmail.com>
2022-01-19 18:58:35 -0600

    Added swapping 3 numbers with Temp

--- src/SwapNumbers.java
+++ src/SwapNumbers.java
@@ -19,7 +19,13 @@ public class SwapNumbers {
 		num1= num2 - num1;
 		
 	}
-	
+	public static void swapThreeNumbersWithTemp() {
+		temp = num1;
+		num1 = num2;
+		num2 = num3;
+		num3 = temp;
+		
+	}
 	
 
 	public static void main(String[] args) {
@@ -43,6 +49,16 @@ public class SwapNumbers {
 		swapTwoNumbersWithoutTemp();
 		System.out.println("num1 = "+ num1);
 		System.out.println("num2 = "+ num2);
+		
+		System.out.println("Before Swapping 3 numbers with Temp");
+		System.out.println("num1 = "+ num1);
+		System.out.println("num2 = "+ num2);
+		System.out.println("num3 = "+ num3);
+		System.out.println("After Swapping 3 numbers with Temp");
+		swapThreeNumbersWithTemp();
+		System.out.println("num1 = "+ num1);
+		System.out.println("num2 = "+ num2);
+		System.out.println("num3 = "+ num3);
 		
 
 	}
