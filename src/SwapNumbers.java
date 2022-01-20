6929f9436cf4497403060efbe0fac4cad18c73f6
Rabin Awal <robinleathal@gmail.com>
2022-01-19 18:46:14 -0600

    Added swapping 2 numbers without temp variable

--- src/SwapNumbers.java
+++ src/SwapNumbers.java
@@ -3,23 +3,47 @@ public class SwapNumbers {
 
 	static int num1 = 100;
 	static int num2 = 200;
+	static int num3 = 300;
+	static int temp = 0;
 	
 	public static void swapTwoNumbersWithTemp() {
-		int temp = 0;
+		
 		temp = num1; 
 		num1= num2;
 		num2= temp;
 		
 	}
+	public static void swapTwoNumbersWithoutTemp() {
+		num1 = num1 - num2;
+		num2 = num1 + num2;
+		num1= num2 - num1;
+		
+	}
+	
+	
 
 	public static void main(String[] args) {
 		System.out.println("Before Swapping");
 		System.out.println("num1 = "+ num1);
 		System.out.println("num2 = "+ num2);
-		System.out.println("After Swapping");
+		System.out.println("num3 = "+ num3);
+		
+		System.out.println("After Swapping with Temp");
+		
 		swapTwoNumbersWithTemp();
 		System.out.println("num1 = "+ num1);
 		System.out.println("num2 = "+ num2);
+		
+		System.out.println("Before Swapping 2 numbers without Temp");
+		System.out.println("num1 = "+ num1);
+		System.out.println("num2 = "+ num2);
+		
+		System.out.println("After Swapping 2 numbers without Temp");
+		
+		swapTwoNumbersWithoutTemp();
+		System.out.println("num1 = "+ num1);
+		System.out.println("num2 = "+ num2);
+		
 
 	}
 
